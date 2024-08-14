cfe14e2a9a97cb0fffa582d46a57c865f8fb8134
mohamed zouari <109849872+Medzvm@users.noreply.github.com>
2024-08-14 14:52:32 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,3 +30,21 @@ document.addEventListener("DOMContentLoaded", function() {
         observer.observe(element);
     });
 });
+
+
+
+document.querySelectorAll('.tab').forEach(tab => {
+    tab.addEventListener('click', () => {
+        const grade = tab.getAttribute('data-grade');
+        
+        document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+        tab.classList.add('active');
+        
+        document.querySelectorAll('.video-section').forEach(section => {
+            section.classList.remove('active');
+        });
+        
+        document.getElementById(`grade-${grade}`).classList.add('active');
+    });
+});
+
